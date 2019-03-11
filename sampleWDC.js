cbb4a6cfa118f3a73690f252da07a54fb6622bdf
amensiko <anastasija.mensikova@trincoll.edu>
2019-03-11 17:25:25 -0400

    Sample WDC -- Asteroid Data

---
+++ sampleWDC.js
@@ -0,0 +1,66 @@
+(function () {
+    var myConnector = tableau.makeConnector();
+
+	myConnector.getSchema = function (schemaCallback) {  //schema = mapping of data
+        console.log("HELLO THERE IM IN SCHEMA");
+		var cols = [{
+        	id: "designation",
+        	dataType: tableau.dataTypeEnum.string
+    	}, {
+        	id: "discovery_date",
+        	dataType: tableau.dataTypeEnum.string
+    	}, {
+        	id: "orbit_class",
+        	dataType: tableau.dataTypeEnum.string
+    	}];
+
+    	var tableSchema = {
+        	id: "asteroids",
+        	alias: "Asteroids and Comets Discovered in the Past 10 Years",
+        	columns: cols
+    	};
+
+    	schemaCallback([tableSchema]);
+	};
+
+    myConnector.getData = function(table, doneCallback) {
+    $.getJSON("https://data.nasa.gov/resource/2vr3-k9wn.json", function(resp) {
+        var featrs = resp.features, 
+            data = [];
+
+        console.log("HELLO THERE IM IN DATA");
+
+        for (var i = 0, len = resp.length; i < len; i++) {
+            data.push({
+                "designation": resp[i].designation,
+                "discovery_date": resp[i].discovery_date,
+                "orbit_class": resp[i].orbit_class
+            });
+            console.log(resp[i]);
+        }
+
+        //console.log(resp);
+
+        // Iterate over the JSON object
+        // for (var i = 0, len = featrs.length; i < len; i++) {
+        //     data.push({
+        //         "designation": featrs[i].designation,
+        //         "discovery_date": featrs[i].discovery_date,
+        //         "orbit_class": featrs[i].orbit_class
+        //     });
+        // }
+
+        table.appendRows(data);
+        doneCallback();
+    });
+};
+
+    tableau.registerConnector(myConnector);
+
+    $(document).ready(function () {      //The jQuery $(document).ready function runs some code when the page loads
+    	$("#submitButton").click(function () {
+        	tableau.connectionName = "Near-Earth Asteroids and Comets";
+        	tableau.submit();
+    	});
+	});
+})();
